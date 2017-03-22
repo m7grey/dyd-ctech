2be0c2e52b0ee14e26a0789e9c44a6e5536e6702
m7grey <m7grey@gmail.com>
2017-03-22 16:49:06 -0400

    class wednesday, to do registration form.

--- js/main.js
+++ js/main.js
@@ -13,29 +13,4 @@ $(document).ready(function(){
 	});
 
 
-
-
-	/*  Advanced search form & Icon  */
-	$('#advanced_search_btn').on("click", function(e){
-		e.preventDefault();
-
-		var ads_box =$('.advanced_search');
-		
-		if(!ads_box.hasClass('advanced_displayed')){
-
-			$(this).addClass('active');
-			ads_box.stop().fadeIn(200).addClass('advanced_displayed');
-
-		}else{
-
-			$(this).removeClass('active');
-			ads_box.stop().fadeOut(200).removeClass('advanced_displayed');
-
-		}
-
-	});
-
-
-
-
 });
